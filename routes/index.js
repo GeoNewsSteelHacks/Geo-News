28105de7ab28d59c559c52090d3151fa18f68155
smccarthy788 <spm64@pitt.edu>
2016-02-20 02:30:32 +0000

    Fake location GET response

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Geo News' });
+});
+
+router.get('/location', function(req, res, next) {
+    res.json({SanFran : { title : "News Story",
+                          description : "There's News!",
+                          siteURL : "https://www.google.com"
+    }});
 });
 
 module.exports = router;
