2e4e677ebd373983c78deb3526808d043b1233a4
kyleplump <kplump1395@gmail.com>
2016-02-20 20:55:41 +0000

    zoom resize circles, mouseover

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,126 +1,79 @@
 var map;
 var initialLocation;
 var cities;
+var cityCircle = new Array(35);
+var index = 0;
+var radius = 85000
+var zoom;
 
 function initialize() {
 
     // INIT BASED OFF LOCATION
     if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-        initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-        map.setCenter(initialLocation);
-     });
-    }
-
-    var mapOptions = {
-      zoom: 5,
-    }
-    
-    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-    
-    // DRAW THE CIRCLES FOR CITIES
-   cities= smallCircles();
-    
-    for (var city in cities) {
-              // Add the circle for this city to the map.
-              var cityCircle = new google.maps.Circle({
-                strokeColor: '#FF0000',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: '#FF0000',
-                fillOpacity: 0.35,
-                map: map,
-                center: cities[city].center,
-                radius: 85000
-              });
-              if(map.getZoom() >11){
-                  var cityCircle = new google.maps.Circle({
-                strokeColor: '#FF0000',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: '#FF0000',
-                fillOpacity: 0.35,
-                map: map,
-                center: cities['nyc'].center,
-                radius: 850
-                });
-                var cityCircle = new google.maps.Circle({
-                strokeColor: '#FF0000',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: '#FF0000',
-                fillOpacity: 0.35,
-                map: map,
-                center: cities['newark'].center,
-                radius: 850
-                });
-              }
-              google.maps.event.addListener(cityCircle, 'mouseover', function(e) {
-            
-            infoWindow.setPosition(e.latLng);
-            infoWindow.open(map);
+        navigator.geolocation.getCurrentPosition(function(position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(initialLocation);
         });
     }
 
-    var infoWindow = new google.maps.InfoWindow({
-        
+    var mapOptions = {
+        zoom: 5,
+    }
+
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    zoom = map.getZoom();
+    // DRAW THE CIRCLES FOR CITIES
+    cities = smallCircles();
+
+    for (var city in cities) {
+        // Add the circle for this city to the map.
+        cityCircle[index] = new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center: cities[city].center,
+            radius: radius
+        });
+        google.maps.event.addListener(cityCircle[index], 'mouseover', function(e) {
+            infoWindow.setPosition(e.latLng);
+            infoWindow.open(map);
+        });
+        index++;
+    }
+    
+     var infoWindow = new google.maps.InfoWindow({
+    
         content: "<div>Click to see news from this region!</div>",
         maxWidth: 500
     });
-    
-    
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-
-//calculates distance between two points in km's
-function calcDistance(p1, p2){
-  return (google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000).toFixed(2);
-}
-
-function findAdjCities(){
-    
-    var allCitiesString = "pittsburgh | new york city | boston | washington dc | baltimore"
-    
-    for(var city in cities){
-        var origin = cities[city],
-        destination = allCitiesString,
-        service = new google.maps.DistanceMatrixService();
-    }
-    
-    service.getDistanceMatrix(
-    {
-        origins: [origin],
-        destinations: [destination],
-        travelMode: google.maps.TravelMode.DRIVING,
-        avoidHighways: false,
-        avoidTolls: false
-    },
-    callback
-    );
-    
-    
-    
-    
-}
 
 
-function callback(response, status) {
-  if (status == google.maps.DistanceMatrixStatus.OK) {
-    var origins = response.originAddresses;
-    var destinations = response.destinationAddresses;
+    google.maps.event.addDomListener(map, 'zoom_changed', function(f) {
 
-    for (var i = 0; i < origins.length; i++) {
-      var results = response.rows[i].elements;
-      for (var j = 0; j < results.length; j++) {
-        var element = results[j];
-        var distance = element.distance.text;
-        var duration = element.duration.text;
-        if(distance >= 190){
-            alert(element);
+        if (zoom < 5) {
+            radius = radius;
         }
-        var from = origins[i];
-        var to = destinations[j];
-      }
-    }
-  }
+        else if (map.getZoom() > zoom) {
+            radius = radius / 2;
+        }
+        else if (map.getZoom() < zoom) {
+            radius = radius * 2;
+        }
+        zoom = map.getZoom();
+        for (var i in cityCircle) {
+            cityCircle[i].setRadius(radius)
+        }
+
+    });
 }
+    
+   
+
+
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
