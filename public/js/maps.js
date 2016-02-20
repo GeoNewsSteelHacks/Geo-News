6112fd4469c07357183157a0cc2a32ce4afff1ce
kyleplump <kplump1395@gmail.com>
2016-02-20 03:51:33 +0000

    set location based on gps

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,26 +1,18 @@
-var geocoder;
 var map;
-var address = 'houston';
-
+var initialLocation;
 function initialize() {
-    geocoder = new google.maps.Geocoder();
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-      }
-    });
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-    var mapOptions = {
-      zoom: 12,
-      center: latlng
-    }
-    
-    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-    
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        map.setCenter(initialLocation);
+     });
+    }
+
+    var mapOptions = {
+      zoom: 10,
+    }
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  
 }
 google.maps.event.addDomListener(window, 'load', initialize);
