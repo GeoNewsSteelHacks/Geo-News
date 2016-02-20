d07e4b673869de550b1761d3a3e930f615c7f5e2
kyleplump <kplump1395@gmail.com>
2016-02-20 16:40:35 +0000

    for cory

--- public/js/maps.js
+++ public/js/maps.js
@@ -17,29 +17,45 @@ function initialize() {
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     
     // DRAW THE CIRCLES FOR CITIES
-    var cities={
+   var cities={
 
-    pittsburgh:{
-        center:{lat: 40.4397, lng: -79.9764},
-    },
-    newYorkCity:{
-        center:{lat: 40.7127, lng: -74.0059}
-    },
-    boston:{
-        center:{lat: 42.3601, lng: -71.0589}
-    },
-    dc:{
-        center:{lat: 38.9047, lng: -77.0164}
-    },
-    baltimore:{
-        center:{lat: 39.2833, lng: -76.6167}
-    },
-    atlanta:{
-        center:{lat: 33.7550, lng: -84.3900}
-    },
-    kansasCity:{
-        center:{lat: 39.0997, lng: -94.5783}
-    }
+        pittsburgh:{
+            center:{lat: 40.4397, lng: -79.9764},
+        },
+        newYorkCity:{
+            center:{lat: 40.7127, lng: -74.0059}
+        },
+        boston:{
+            center:{lat: 42.3601, lng: -71.0589}
+        },
+        dc:{
+            center:{lat: 38.9047, lng: -77.0164}
+        },
+        baltimore:{
+            center:{lat: 39.2833, lng: -76.6167}
+        },
+        atlanta:{
+            center:{lat: 33.7550, lng: -84.3900}
+        },
+        kansasCity:{
+            center:{lat: 39.0997, lng: -94.5783}
+        },
+        austin:{
+            center:{lat: 30.2500, lng: -97.7500}
+        },
+        cincinnati:{
+            center:{lat: 39.1000, lng: -84.5167}
+        },
+        stPaul:{
+            center:{lat: 44.9442, lng: -93.0936}
+        },
+        indianapolis:{
+            center:{lat: 39.7910, lng: -86.1480}
+        },
+        charlotte:{
+            center:{lat: 35.2269, lng: -80.8433}
+        }
+    
     
     
     };
