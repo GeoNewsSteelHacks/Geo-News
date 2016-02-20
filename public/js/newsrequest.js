c1e6dd2d94f14624b65cf7647e33d7b3942a0d78
Cory Trbojevic <cmt75@pitt.edu>
2016-02-20 18:16:43 -0500

    locations

---
+++ public/js/newsrequest.js
@@ -0,0 +1,10 @@
+function newsRequest(locationName){
+	$.get("/location", {location:locationName}, function(data){
+		for(var i = 0; i < data.length; i++){
+			var elementContainer = '';
+			elementContainer = elementContainer + '<div>' 
+				+ '<h1>' + data[i].title + '</h1>'
+				+ '<p>' + data[i].description + '</p>' 
+				+ '<p>' + data[i].url + '</p>' + '</div>'
+			}
+	}
