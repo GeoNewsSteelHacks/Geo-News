3d694d87699235e15a67eb47c21f6d751ce23f71
kyleplump <kplump1395@gmail.com>
2016-02-21 01:48:27 +0000

    geez

--- public/js/maps.js
+++ public/js/maps.js
@@ -24,10 +24,38 @@ function initialize() {
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
     // DRAW THE CIRCLES FOR CITIES
-    
-       
-     cities= cityCenters();
-    
+    drawCircles();
+
+    google.maps.event.addDomListener(map, 'zoom_changed', function(f) {
+
+        if (map.getZoom() < 5) {
+            for (var r in cityCircle) {
+                cityCircle[r].setVisible(false)
+            }
+
+            drawFusionLayer('1H1Ee3_2xX7wr25WNRWs3uXyGLzTtjxnDUUMrozWt');
+
+        }
+        else if (map.getZoom() >= 5) {
+            for (var z in cityCircle) {
+                cityCircle[z].setMap(null)
+                cityCircle[z] = null
+            }
+            drawCircles()
+
+
+        }
+
+
+
+    });
+
+}
+
+
+function drawCircles() {
+    cities = cityCenters();
+
 
     for (var city in cities) {
         // Add the circle for this city to the map.
@@ -41,121 +69,88 @@ function initialize() {
             center: cities[city].center,
             radius: radius
         });
+
         // event handler for hover
         google.maps.event.addListener(cityCircle[index], 'mouseover', function(e) {
             infoWindow.setPosition(e.latLng);
             infoWindow.open(map);
         });
-        google.maps.event.addListener(cityCircle[index], 'mouseout', function(e){
-        
-            infoWindow.close(map);
-        });
-        // event handler for click
-        google.maps.event.addListener(cityCircle[index], 'click', function(t){
-           $(document).ready(function(){
+        google.maps.event.addListener(cityCircle[index], 'mouseout', function(e) {
+
+                infoWindow.close(map);
+            })
+            // event handler for click
+        google.maps.event.addListener(cityCircle[index], 'click', function(t) {
+            $(document).ready(function() {
 
                 $("#panel").text("Hello world!");
 
-   
+
             });
         });
-        
-        
         index++;
     }
-    
-     var infoWindow = new google.maps.InfoWindow({
-    
+
+    var infoWindow = new google.maps.InfoWindow({
+
         content: "<div>Click to see news from this region!</div>",
         maxWidth: 500
     });
 
 
-    google.maps.event.addDomListener(map, 'zoom_changed', function(f) {
-      
-        if (map.getZoom() < 5) {
-            for(var r in cityCircle){
-                cityCircle[r].setVisible(false)
-            }
-            var eastCoords = [
-            {lat:35.0041, lng:-88.1955},
-            {lat:34.9918, lng:-85.6068},
-            {lat:32.8404, lng:-85.1756},
-            {lat:32.2593, lng:-84.8927},
-            ];
-            bermudaTriangle = new google.maps.Polygon({
-                paths: eastCoords,
-                strokeColor: '#FF0000',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: '#FF0000',
-                fillOpacity: 0.35
-            });
-            
-            bermudaTriangle.setMap(map);
-
-
-            
-        }
-        else if (map.getZoom() >= 5) {
-            bermudaTriangle.setMap(null)
-            for(var t in cityCircle){
-                cityCircle[t].setVisible(true)
-            }
-           
-            radius = 42500;
-            for (var k in cityCircle) {
-                cityCircle[k].setRadius(radius)
-            }
-        }
-        
-        
-
-    });
 }
 
-      
-    function drawFusionLayer(fusionTable){
-        var layer = new google.maps.FusionTablesLayer({
-            query: {
-              select: '\'geometry\'',
-              from: fusionTable
-            },
-            styles: [{ where: "'style' = 0",
-                polygonOptions: {
-                    fillColor: '#FF0000'
-                }    
-                },{ where: "'style' = 1",
-                polygonOptions: {
-                    fillColor: '#FF7F00'
-                }    
-                },{ where: "'style' = 2",
-                polygonOptions: {
-                    fillColor: '#FFFF00'
-                }    
-                },{ where: "'style' = 3",
-                polygonOptions: {
-                    fillColor: '#00FF00'
-                }    
-                },{ where: "'style' = 4",
-                polygonOptions: {
-                    fillColor: '#0000FF'
-                }    
-                },{ where: "'style' = 5",
-                polygonOptions: {
-                    fillColor: '#4B0082'
-                }    
-                }]
-          });
-        layer.setMap(map);
-        currentOverlay = map;
-    }  
-    
-    drawFusionLayer('1H1Ee3_2xX7wr25WNRWs3uXyGLzTtjxnDUUMrozWt');
-    
+
+function drawFusionLayer(fusionTable) {
+    var layer = new google.maps.FusionTablesLayer({
+        query: {
+            select: '\'geometry\'',
+            from: fusionTable
+        },
+        styles: [{
+            where: "'style' = 0",
+            polygonOptions: {
+                fillColor: '#FF0000'
+            }
+        }, {
+            where: "'style' = 1",
+            polygonOptions: {
+                fillColor: '#FF7F00'
+            }
+        }, {
+            where: "'style' = 2",
+            polygonOptions: {
+                fillColor: '#FFFF00'
+            }
+        }, {
+            where: "'style' = 3",
+            polygonOptions: {
+                fillColor: '#00FF00'
+            }
+        }, {
+            where: "'style' = 4",
+            polygonOptions: {
+                fillColor: '#0000FF'
+            }
+        }, {
+            where: "'style' = 5",
+            polygonOptions: {
+                fillColor: '#4B0082'
+            }
+        }]
+    });
+    layer.setMap(map);
+    currentOverlay = map;
+    google.maps.event.addListener(map, 'zoom_changed', function() {
+        if (map.getZoom() >= 5) {
+            layer.setMap(null)
+        }
+    });
+
+}
+
+
+
 
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
-
-
