cca4156589b483a0c9529979331c849e70d04ca5
smccarthy788 <spm64@pitt.edu>
2016-02-21 07:14:54 +0000

    formatted city names

--- public/js/cityCenters.js
+++ public/js/cityCenters.js
@@ -2,7 +2,7 @@ function cityCenters(){
     return {
         pittsburgh:{
             center:{lat: 40.4397, lng: -79.9764},
-            name: 'pittsburgh'
+            name: 'Pittsburgh'
         },
         newYorkCity:{
             center:{lat: 40.7127, lng: -74.0059},
@@ -14,111 +14,111 @@ function cityCenters(){
         },
         dc:{
             center:{lat: 38.9047, lng: -77.0164},
-            name: 'washington dc'
+            name: 'Washington DC'
         },
         baltimore:{
             center:{lat: 39.2833, lng: -76.6167},
-            name: 'baltimore'
+            name: 'Baltimore'
         },
         atlanta:{
             center:{lat: 33.7550, lng: -84.3900},
-            name: 'atlanta'
+            name: 'Atlanta'
         },
         kansasCity:{
             center:{lat: 39.0997, lng: -94.5783},
-            name: 'kansas city'
+            name: 'Kansas City'
         },
         austin:{
             center:{lat: 30.2500, lng: -97.7500},
-            name: 'austin'
+            name: 'Austin'
         },
         cincinnati:{
             center:{lat: 39.1000, lng: -84.5167},
-            name: 'cincinnati'
+            name: 'Cincinnati'
         },
         stPaul:{
             center:{lat: 44.9442, lng: -93.0936},
-            name: 'st. paul'
+            name: 'St. Paul'
         },
         indianapolis:{
             center:{lat: 39.7910, lng: -86.1480},
-            name: 'indianapolis'
+            name: 'Indianapolis'
         },
         charlotte:{
             center:{lat: 35.2269, lng: -80.8433},
-            name: 'charlotte'
+            name: 'Charlotte'
         },
         denver:{
             center:{lat: 39.7392, lng: -104.9903},
-            name: 'denver'
+            name: 'Denver'
         },
         tucson:{
             center:{lat: 32.2217, lng: -110.9264},
-            name: 'tucson'
+            name: 'Tucson'
         },
         elPaso:{
             center:{lat: 31.7903, lng: -106.4233},
-            name: 'el paso'
+            name: 'El Paso'
         },
         miami:{
             center:{lat: 25.7753, lng: -80.2089},
-            name: 'miami'
+            name: 'Miami'
         },
         newOrleans:{
             center:{lat: 29.9500, lng: -90.0667},
-            name: 'new orleans'
+            name: 'New Orleans'
         },
         chicago:{
             center:{lat: 41.8369, lng: -87.6847},
-            name: 'chicago'
+            name: 'Chicago'
         },
         phoenix:{
             center:{lat: 33.4500, lng: -112.0667},
-            name: 'phoenix'
+            name: 'Phoenix'
         },
         la:{
             center:{lat: 34.0500, lng: -118.2500},
-            name: 'los angeles'
+            name: 'Los Angeles'
         },
         seattle:{
             center:{lat: 47.6097, lng: -122.3331},
-            name: 'seattle'
+            name: 'Seattle'
         },
         oklahoma:{
             center:{lat: 35.4822, lng: -97.5350},
-            name: 'oklahoma'
+            name: 'Oklahoma'
         },
         philadelphia:{
             center:{lat: 39.9500, lng: -75.1667},
-            name: 'philadelphia'
+            name: 'Philadelphia'
         },
         dallas:{
             center:{lat: 32.7767, lng: -96.7970},
-            name: 'dallas'
+            name: 'Dallas'
         },
         columbus:{
             center:{lat: 39.9833, lng: -82.9833},
-            name: 'columbus'
+            name: 'Columbus'
         },
         vegas:{
             center:{lat: 36.1215, lng: -115.1739},
-            name: 'las vegas'
+            name: 'Las Vegas'
         },
         albuquerque:{
             center:{lat: 35.1107, lng: -106.6100},
-            name: 'albuquerque'
+            name: 'Albuquerque'
         },
         detroit:{
             center:{lat: 42.3314, lng: -83.0458},
-            name: 'detroit'
+            name: 'Detroit'
         },
         saltLake:{
             center:{lat: 40.7500, lng: -111.8833},
-            name: 'salt lake city'
+            name: 'Salt Lake City'
         },
         portland:{
             center:{lat: 45.5200, lng: -122.6819},
-            name: 'portland'
+            name: 'Portland'
         }
     };
 }
