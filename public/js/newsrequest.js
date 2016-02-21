1cdc3d53267faf9728635fbf8952f609cc787e3d
smccarthy788 <spm64@pitt.edu>
2016-02-21 01:14:54 +0000

    working side pannel filler

--- public/js/newsrequest.js
+++ public/js/newsrequest.js
@@ -1,14 +1,11 @@
 function newsRequest(locationName){
 	$.get("/location", {location:locationName}, function(data){
-		//alert(data.name);
 		for(var i = 0; i < data.name.length; i++){
 			var elementContainer = '<div>' 
 				+ '<h1>' + data.name[i].title + '</h1>'
-				+ '<p>' + data[i].description + '</p>' 
-				+ '<a href='> + data[i].url + <'/>' + '</div>';
-			alert(elementContainer);
+				+ '<p>' + data.name[i].description + '</p>' 
+				+ '<a href=' + data.name[i].url + '>' + '</>' + '</div>';
 			$("#panel").append(elementContainer);
-		};
+		}
 	});
 }
-
