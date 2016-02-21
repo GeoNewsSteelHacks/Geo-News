dae77dcbd7719f0af22c25a49e2030a94bea02e0
smccarthy788 <spm64@pitt.edu>
2016-02-21 02:14:07 +0000

    Regional fusion table

--- public/js/maps.js
+++ public/js/maps.js
@@ -33,7 +33,7 @@ function initialize() {
                 cityCircle[r].setVisible(false);
             }
 
-            drawFusionLayer('1H1Ee3_2xX7wr25WNRWs3uXyGLzTtjxnDUUMrozWt');
+            drawFusionLayer('187xl9kj6GDMkR3AEmhXtorHVZbcxJ6AiAikXx0eC');
 
         }
         else if (map.getZoom() >= 5) {
